7f98aac0d69a7afe9d73ae7bfd43adfebfeb662a
Jordan Negreiros <jordannegreirossantos@gmail.com>
2020-03-04 19:25:13 -0300

    Rotas de Edição e Cadastro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
+import {BrowserModule} from '@angular/platform-browser';
 import {ModuleWithProviders, NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import {FormsModule} from '@angular/forms'
-import {HttpClientModule} from '@angular/common/http';/* Requisições Ajax */
-import { HomeComponent } from './home/home.component';
-import {Routes, RouterModule} from '@angular/router';
-import { LoginComponent } from './login/login.component';
+import {AppComponent} from './app.component';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http'; /* Requisições Ajax */
+import {HomeComponent} from './home/home.component';
+import {RouterModule, Routes} from '@angular/router';
+import {LoginComponent} from './login/login.component';
 import {HttpInterceptorModule} from './service/header-interceptor.service';
-import { UsuarioComponent } from './componente/usuario/usuario.component';
+import {UsuarioComponent} from './componente/usuario/usuario.component';
+import {UsuarioAddComponent} from './componente/usuario/usuario-add/usuario-add.component';
 
 export const appRouters: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'login', component: LoginComponent},
   {path: '', component: LoginComponent},
-  {path: 'usuarioList', component: UsuarioComponent}
+  {path: 'usuarioList', component: UsuarioComponent},
+  {path: 'usuarioAdd', component: UsuarioAddComponent},
+  {path: 'usuarioAdd/:id', component: UsuarioAddComponent}
   ];
 
 export const routes: ModuleWithProviders = RouterModule.forRoot(appRouters);
@@ -24,7 +27,8 @@ export const routes: ModuleWithProviders = RouterModule.forRoot(appRouters);
     AppComponent,
     HomeComponent,
     LoginComponent,
-    UsuarioComponent
+    UsuarioComponent,
+    UsuarioAddComponent
   ],
   imports: [
     BrowserModule,
