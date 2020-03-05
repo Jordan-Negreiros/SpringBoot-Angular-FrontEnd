d2048f29c57afab45bbdf0211f63caeede02607f
Jordan Negreiros <jordannegreirossantos@gmail.com>
2020-03-05 00:18:33 -0300

    Implementando validações no cadastro e login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {LoginServiceService} from '../service/login-service.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,7 +11,7 @@ export class LoginComponent implements OnInit {
 
   usuario = {login: '', senha: ''}
 
-  constructor(private loginService: LoginServiceService) {
+  constructor(private loginService: LoginServiceService, private router: Router) {
   }
 
   public login() {
@@ -19,6 +20,9 @@ export class LoginComponent implements OnInit {
 
 
   ngOnInit() {
+    if (localStorage.getItem('token') !== null && localStorage.getItem('token').toString().trim() !== null) {
+      this.router.navigate(['home']);
+    }
   }
 
 }
